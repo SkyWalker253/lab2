c1a3992baededfbc44c51254b5483b29399fb74e
Aydar <125701389+SkyWalker253@users.noreply.github.com>
2023-03-02 15:24:59 +0300

    Добавьте файлы проекта.

---
+++ lab2/Person.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab2
+{
+    internal class Person
+    {
+        private char gen;
+
+        public string Name
+        {
+            get;
+            set;
+        }
+        public string Surname
+        {
+            get;
+            set;
+        }
+        public DateTime Bday
+        {
+            get;
+            set;
+        }
+        public char Gen
+        {
+            get { return gen; }
+            set { gen = value; }
+        }
+        public Person(char gen, string name, string surname, DateTime bday)
+        {
+            Gen = gen;
+            Name = name;
+            Surname = surname;
+            Bday = bday;
+        }
+        public Person()
+        {
+        }
+        public void Print()
+        {
+            Console.WriteLine($"Имя: {Name} Фамилия: {Surname} Дата рождения: {Bday} Пол: {Gen}");
+        }
+        public static Person Input()
+        {
+            Console.WriteLine("Введите имя:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Введите фамилию: ");
+            string surname = Console.ReadLine();
+            Console.WriteLine("Введите дату рождения через точку: ");
+            DateTime bday = Convert.ToDateTime(Console.ReadLine());
+            Console.WriteLine("Введите пол: ");
+            char gen = Convert.ToChar(Console.ReadLine());
+            return new Person(gen,name, surname, bday);
+        }
+        public override string ToString()
+        {
+            return ($"Фамилия: {Surname} Имя: {Name} Дата рождения: {Bday} Пол: {Gen}");
+        }
+        public int Nowage()
+        {
+            DateTime today = DateTime.Now;
+            int age = today.Year - Bday.Year;
+            if (today.Month == Bday.Month && today.Day < Bday.Day || today.Month < Bday.Month)
+            { age--; }
+            return age;
+        }
+    }
+}
---
+++ lab2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Person Emil = new Person('m', "Emil", "Bildanov", new DateTime(2005, 11, 6));
+            //Emil.Print();
+            //Person Aydar = new Person();
+            //Aydar.Input();
+            //Aydar.Print();
+            //Console.WriteLine(Emil.ToString());
+            //Console.WriteLine(Emil.Nowage());
+
+            Console.WriteLine("Введите количество людей:");
+            int n = int.Parse(Console.ReadLine());
+
+            Person[] arr = new Person[n];
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"Введите данные о {i + 1} человеке:");
+                arr[i] = Person.Input();
+            }
+
+            Console.WriteLine("Люди:");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine($"Имя:{arr[i].Name}\nФамилия: {arr[i].Surname.Substring(0, 1)}\nВозраст: {arr[i].Nowage()}\nГенден: {arr[i].Gen}\n");
+            }
+        }
+    }
+}
